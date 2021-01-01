e74ec99736c6b3c69de9e44a9d7f6e33debb6857
Saad Guessous <guessous_saad@hotmail.com>
2021-01-01 18:56:59 +0200

    initial commit

---
+++ src/core/sso_clojure.clj
@@ -0,0 +1,1 @@
+(ns core.sso-clojure)
---
+++ test/core/sso_clojure_test.clj
@@ -0,0 +1,7 @@
+(ns core.sso-clojure-test
+  (:require [clojure.test :refer :all]
+            [core.sso-clojure :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
